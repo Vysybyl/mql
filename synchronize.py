dba412777536f9d74dc9d82b113ff790220f8670
Vysybyl <kanthavel84@gmail.com>
2016-04-17 11:33:26 +0100

    Corrected Synch script with new paths.

--- synchronize.py
+++ synchronize.py
@@ -1,12 +1,10 @@
 from pythonutils.filemanager import manager as mgr
 
-origindir = "C:\\Git\\mql"
-destinationdir1 = "C:\\Users\\Kanthavel\\AppData\\Roaming\\MetaQuotes\\Terminal\\A6DFBB1B8DE9672D2328FF3445436DEC\\MQL4"
-destinationdir2 = "C:\\Users\\Kanthavel\\AppData\\Roaming\\MetaQuotes\\Terminal\\50CA3DFB510CC5A8F28B48D1BF2A5702\\MQL4"
+origindir = "C:\\Users\\Kanthavel\\AppData\\Roaming\\MetaQuotes\\Terminal\\A6DFBB1B8DE9672D2328FF3445436DEC\\MQL4"
+destinationdir = "C:\\Users\\Kanthavel\\AppData\\Roaming\\MetaQuotes\\Terminal\\50CA3DFB510CC5A8F28B48D1BF2A5702\\MQL4"
 
 def main():
-    mgr.sync(origindir,destinationdir1)
-    mgr.sync(origindir,destinationdir2)
+    mgr.sync(origindir,destinationdir)
 
 if __name__ == "__main__":
     main()
